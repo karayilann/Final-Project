c5097c511c06b8a597081d32a79f197cf3999217
Muhammet Karayilan <muhammethamitkarayilan@gmail.com>
2023-12-20 23:03:09 +0300

    Character Movement added
    Karakterin sağa sola hareketi yazıldı ve zıplama fonksiyonu düzenlendi.

--- FinalProject/Assets/Scripts/Character/Movement.cs
+++ FinalProject/Assets/Scripts/Character/Movement.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpFunction : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float jumpPower;
     public float jumpGravity;
+    public float movementSpeed;
 
     public Rigidbody2D rb2d;
 
     bool isGround = false;
 
-    void Update()
+    void FixedUpdate()
     {
         CharactersJump();
+        CharactersMovement();
     }
 
     /// <summary>
@@ -28,10 +30,27 @@ public class JumpFunction : MonoBehaviour
         }
         else if(rb2d.velocity.y > 0)
         {
-            rb2d.gravityScale = 10f;
+            rb2d.gravityScale = 5f;
         }
     }
 
+    /// <summary>
+    /// Karakterin hareket etmesi için yazýlan kodlar.
+    /// </summary>
+    private void CharactersMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb2d.velocity = Vector2.left * movementSpeed;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb2d.velocity = Vector2.right * movementSpeed;
+        }
+    }
+
+
+    #region Zemin Kontrol
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -47,4 +66,7 @@ public class JumpFunction : MonoBehaviour
             isGround = false;
         }
     }
+    #endregion
+
+
 }
