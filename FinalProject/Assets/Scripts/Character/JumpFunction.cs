5491e308664a8e75a8cf9f49e9e767276011cb10
Muhammet Karayilan <muhammethamitkarayilan@gmail.com>
2023-12-20 18:25:06 +0300

    Character's Jump Mechanics is created

---
+++ FinalProject/Assets/Scripts/Character/JumpFunction.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpFunction : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpPower;
+    public float jumpGravity;
+
+    public Rigidbody2D rb2d;
+
+    bool isGround = false;
+
+    void Update()
+    {
+        CharactersJump(); 
+    }
+
+    /// <summary>
+    /// Karakterin zýplama fonksiyonunu içerir.
+    /// </summary>
+    void CharactersJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround) 
+        {
+            rb2d.gravityScale = jumpGravity;
+            rb2d.velocity = Vector3.up * jumpPower;
+        }
+        else if(!isGround)
+        {
+            rb2d.gravityScale = 1f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = false;
+        }
+    }
+}
