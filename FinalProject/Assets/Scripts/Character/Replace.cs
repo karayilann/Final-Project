1a02050ba22c9b02ce3762fa92bd54a1573b97ec
Muhammet Karayilan <muhammethamitkarayilan@gmail.com>
2023-12-24 03:32:04 +0300

    Character's swap places
    Belirli aralıklarla karakterler yer değiştirecek olan kodu yazıldı

---
+++ FinalProject/Assets/Scripts/Character/Replace.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class Replace : MonoBehaviour
+{
+    public Transform character1;
+    public Transform character2;
+    public Vector3 characterPos1;
+    int minute, seconds;
+
+    [SerializeField] TextMeshProUGUI timer;
+
+    float elapsedTime;
+
+    private void Start()
+    {
+        InvokeRepeating("ChangePosition", 60f, Random.Range(30,60));
+        //InvokeRepeating("ChangePosition", 60f,60f);  60 sn geçtikten sonra 60sn de bir karakterlerin yerleri deðiþecek.
+    }
+
+    void Update()
+    {
+        Timer();
+    }
+
+    /// <summary>
+    /// Karakterin 60 sn aralýklarla karakterlerin yer deðiþmesini saðlayacak kod
+    /// </summary>
+    private void ChangePosition()
+    {
+        characterPos1 = character1.position;
+        character1.position = character2.position;
+        character2.position = characterPos1;
+        Debug.Log("Deðiþti" + " " + minute + " " + seconds);
+    }
+
+    private void Timer()
+    {
+        elapsedTime += Time.deltaTime;
+        minute = Mathf.FloorToInt(elapsedTime / 60);
+        seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timer.text = string.Format("Time: {0:00}:{1:00}", minute, seconds);
+    }
+}
